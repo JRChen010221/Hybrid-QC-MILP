4abf880094a306a8d3ad61643d28fcd29f9d2892
JRChen010221 <13671262721@163.com>
2024-03-04 14:22:26 +0800

    fix display bug and add black edges to the bars

--- visualiaztion.py
+++ visualiaztion.py
@@ -39,22 +39,21 @@ def plot_gantt_chart(data: JobshopData, job_num: int, machine_num: int, task: st
     colors = [mcolors.CSS4_COLORS[random.choice(list(mcolors.CSS4_COLORS.keys()))] for _ in range(job_num)]
 
     for i in range(instance_num):
-        x = np.array(xs[i])
-        t = np.array(ts[i])
+        x = xs[i]
+        t = ts[i]
         p_arr, _, _, _ = data.instances[i]
         fig, ax = plt.subplots(figsize=(10, 15))
 
         for j in range(job_num):
-            m = np.nonzero(x[j])[0].item()
-            ax.broken_barh([(t[j], p_arr[j, m])], (m-0.2, 0.4), facecolors=colors[j])
+            ms = np.array([x[f'x[{j+1}, {k+1}]'] for k in range(machine_num)], dtype=np.int8)
+            m = np.nonzero(ms)[0].item()
+            ax.broken_barh([(t[f't[{j+1}]'], p_arr[j, m])], (m-0.1, 0.2), facecolors=colors[j], edgecolor='black')
 
         ax.set_xlabel('Time')
         ax.set_ylabel('Machine')
         ax.set_title('Gantt Chart of Job Shop Scheduling')
-        ax.set_yticks(range(1, machine_num+1))
+        ax.set_yticks(range(machine_num))
         ax.set_yticklabels([f'Machine {i}' for i in range(1, machine_num+1)])
-        ax.set_xlim(0, 250)
-        ax.set_ylim(0.5, machine_num + 0.5)
 
         legend_patches = [patches.Patch(color=colors[i], label=f'Job {i+1}') for i in range(job_num)]
         ax.legend(handles=legend_patches, bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0., ncol=2)
@@ -65,8 +64,3 @@ def plot_gantt_chart(data: JobshopData, job_num: int, machine_num: int, task: st
         save_pth = os.path.join(plot_pth, f"{task}_{job_num}_{machine_num}_{i+1}.png")
         plt.savefig(save_pth)
         plt.show()
-
-
-if __name__ == '__main__':
-    data = JobshopData(120, 90)
-    plot_gantt_chart(data, 120, 90, "hybrid")
